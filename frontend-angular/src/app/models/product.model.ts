4aa9821a5b2ec99cc80669cfd938631c37040599
Ronald Manzano <md23005@ues.edu.sv>
2025-11-04 21:16:41 -0600

    Revert "New feature#2"

--- frontend-angular/src/app/models/product.model.ts
+++
@@ -1,7 +0,0 @@
-export interface Product {
-  id?: number;         // opcional porque al crear no hay id todavía
-  nombre: string;
-  descripcion?: string;
-  precio: number;
-  imagenUrl?: string;
-}
